001aaf345d56265af0788947bd9aefce3669bd09
Shakhov Aleksandr <None-Value@yandex.ru>
2025-09-19 00:21:55 +0900

    documentations added

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,6 @@ class Products  {
 
     protected goodsOnSite : IProduct[] = [];
     protected goodPresentation? : IProduct;
-    // protected good : IProduct;
     
     setItems(goods: IProduct[]) {
         this.goodsOnSite = goods;
@@ -53,10 +52,12 @@ class Bascet {
     protected goodsOnBascet : IProduct[] = [];
 
     setItemToBascet(good : IProduct) {
+        console.log("Товар добавлен в корзину");
         this.goodsOnBascet.push(good);
     }
 
     getItemsFromBascet() : IProduct[] {
+        // console.log("Содержимое казины:");
         return this.goodsOnBascet;
     }
 
@@ -65,6 +66,7 @@ class Bascet {
     }
 
     clearBascet() {
+        console.log("Корзина очищена");
         this.goodsOnBascet = [];
     }
 
@@ -78,21 +80,60 @@ class Bascet {
     return totalSum;
     }
 
+    countItems(): number {
+        return this.goodsOnBascet.length;
+    }
+
+    isInBascet (id : string): boolean  {
+        if (this.goodsOnBascet.find(good => good.id === id))  return true
+        else return false;
+    }
+
     
 }
 
 const list = new Products();
+// setItems - метод для сохранения массива товаров полученного в параметрах метода
 list.setItems(apiProducts.items);
+
+// getItems - метод для получение массива товаров из модели
 console.log('Массив товаров из каталога: ', list.getItems());
-// const rend = list.getItems();
-// rend.forEach(item => console.log(item))
-// console.log(list.findGoodByID("854cef69-976d-4c2a-a18c-2aa45046c390"));
+
+// findGoodByID - метод получение одного товара по его id
+console.log('Товар с ID: ', list.findGoodByID("c101ab44-ed99-4a54-990d-47aa2bb4e7d9"));
+
+// setToShow - метод для сохранения товара для подробного отображения
+console.log('Товар сохранен для подробного отображения', list.setToShow(list.findGoodByID("c101ab44-ed99-4a54-990d-47aa2bb4e7d9")));
+
+// getToShow - метод для получение товара для подробного отображения
+console.log('Вывод товара с подробностями:', list.getToShow());
+
 
 const bascet = new Bascet();
+// setItemToBascet -  метод для добавления товара, который был получен в параметре в массив корзины
 bascet.setItemToBascet(list.findGoodByID("854cef69-976d-4c2a-a18c-2aa45046c390"));
-// bascet.getItemsFromBascet();s
-console.log(bascet.getItemsFromBascet());
 
-bascet.setItemToBascet(list.findGoodByID("c101ab44-ed99-4a54-990d-47aa2bb4e7d9"));
+// getItemsFromBascet - метод для получения массива товаров, которые находятся в корзине
+console.log("Содержимое корзины", bascet.getItemsFromBascet());
 
-console.log(bascet.getItemsFromBascet());
+// removeItemFromBascet - метод для удаления товара, полученного в параметре из массива корзины
+bascet.removeItemFromBascet(list.findGoodByID("854cef69-976d-4c2a-a18c-2aa45046c390"));
+console.log("Содержимое корзины", bascet.getItemsFromBascet());
+
+// clearBascet - метод для очистки корзины
+bascet.setItemToBascet(list.findGoodByID("412bcf81-7e75-4e70-bdb9-d3c73c9803b7"));
+bascet.setItemToBascet(list.findGoodByID("854cef69-976d-4c2a-a18c-2aa45046c390"));
+bascet.clearBascet();
+console.log("Содержимое корзины", bascet.getItemsFromBascet());
+
+// calculateSumm - метод для подсчета суммы 
+bascet.setItemToBascet(list.findGoodByID("412bcf81-7e75-4e70-bdb9-d3c73c9803b7"));
+bascet.setItemToBascet(list.findGoodByID("854cef69-976d-4c2a-a18c-2aa45046c390"));
+console.log("Сумма заказа: ", bascet.calculateSumm());
+
+// countItems - метод для получения количества товаров в корзине
+console.log("Количество товаров в корзине: ", bascet.countItems());
+
+// isInBascet - метод для проверки наличия товара в корзине по его id, полученному в параметр метода 
+console.log("Товар в корзине: ", bascet.isInBascet("854cef69-976d-4c2a-a18c-2aa45046c390"));
+
