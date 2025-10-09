9b79dab0209ab8781bb076da3dff6c5c139ff064
Shakhov Aleksandr <None-Value@yandex.ru>
2025-10-09 23:57:30 +0900

    feat: some structure for start page realised

--- src/components/base/Component.ts
+++ src/components/base/Component.ts
@@ -1,6 +1,12 @@
 /**
  * Базовый компонент
  */
+import {Products} from './components/base/Models/Products.ts'
+import {IProduct} from '../../types/index.ts';
+import {ensureElement} from '../../utils/utils.ts'
+
+
+
 export abstract class Component<T> {
     protected constructor(protected readonly container: HTMLElement) {
         // Учитывайте что код в конструкторе исполняется ДО всех объявлений в дочернем классе
@@ -18,9 +24,115 @@ export abstract class Component<T> {
         }
     }
 
+
+    protected setText(element: HTMLElement | null, value: unknown) {
+		if (element) {
+            // console.log(element)
+			element.textContent = String(value);
+		}
+	}
+
+
+
     // Вернуть корневой DOM-элемент
     render(data?: Partial<T>): HTMLElement {
         Object.assign(this as object, data ?? {});
+        // console.log(this.container)
         return this.container;
     }
+}
+
+
+
+interface IGallaryData {
+    catalog: HTMLElement;
+}
+
+export class Card extends Component<IProduct> {
+    // catalogElement: HTMLElement;
+    private cardID: string = '';
+    private cardCategory: HTMLElement | null;
+    private cardDescription: HTMLElement | null;
+    private cardButton: HTMLButtonElement| null;
+    private cardTitle: HTMLElement;
+    private cardImage: HTMLImageElement | null;
+    private cardPrice: HTMLElement | null;
+
+    	Category: { [key: string]: string } = {
+		'софт-скил': 'card__category_soft',
+		'хард-скил': 'card__category_hard',
+		'дополнительное': 'card__category_additional',
+		'другое': 'card__category_other',
+		'кнопка': 'card__category_button',
+	};
+
+    constructor(container: HTMLElement, ) {
+        super(container);
+        this.cardCategory = container.querySelector('.card__category')
+        this.cardTitle = ensureElement<HTMLElement>('.card__title', container);
+        this.cardImage = container.querySelector('.card__image')
+        this.cardPrice = container.querySelector('.card__price')
+        this.cardDescription = container.querySelector('.card__text');
+        this.cardButton = container.querySelector('.card__button');
+
+
+        this.cardCatalog = ensureElement<HTMLElement>('.gallery');
+    }
+
+    toggleClass(element: HTMLElement, className: string, force?: boolean) {
+		element.classList.toggle(className, force);
+	}
+
+
+    set id(value: string){
+		this.cardID = value;
+	}
+
+    get id(): string {
+		return this.cardID || '';
+	}
+
+    set title(value: string) {
+		this.setText(this.cardTitle, value);
+	}
+
+	// set image(value: string) {
+	// 	this.setImage(this.cardImage, value, this.cardTitle);
+	// }
+    set image(value: string) 
+    {
+        if (this.cardImage instanceof HTMLImageElement) 
+        { 
+            this.setImage(this.cardImage, value, this.title); 
+        } 
+    }
+
+
+	set description(value: string) {
+		this.setText(this.cardDescription, value);
+	}
+
+	set category(value: string) {
+        if (this.cardCategory instanceof HTMLImageElement) 
+        { 
+            this.setText(this.cardCategory, value);
+    		this.toggleClass(this.cardCategory, this.Category[value], true);
+        } 
+		
+	}
+
+	set price(value: number | null) {
+		this.setText(
+			this.cardPrice,
+			value ? `${value.toString()} синапсов` : 'Бесценно'
+		);
+		if (value === null && this.cardButton) {
+			this.cardButton.disabled = true;
+		}
+	}
+
+	set button(value: string) {
+		this.setText(this.cardButton, value);
+	}
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,87 +1,89 @@
 import './scss/styles.scss';
+import {cloneTemplate, ensureElement} from './utils/utils';
 import {apiProducts} from './utils/data.ts';
 import {API_URL} from './utils/constants.ts';
+import {Card} from './components/base/Component.ts';
 import {Products} from './components/base/Models/Products.ts'
 import {Basket} from './components/base/Models/Basket.ts'
 import {Buyer} from './components/base/Models/Buyer.ts'
 import {Api} from './components/base/Api.ts'
 import {Requests} from './components/base/Api.ts'
 
-console.log('ТЕСТ КЛАССА Products');
+// console.log('ТЕСТ КЛАССА Products');
 
 const list = new Products();
-// setItems - метод для сохранения массива товаров полученного в параметрах метода
-list.setItems(apiProducts.items);
+// // setItems - метод для сохранения массива товаров полученного в параметрах метода
+// list.setItems(apiProducts.items);
 
-// getItems - метод для получение массива товаров из модели
-console.log('\n 1 Массив товаров из каталога: ', list.getItems());
+// // getItems - метод для получение массива товаров из модели
+// console.log('\n 1 Массив товаров из каталога: ', list.getItems());
 
-// findGoodByID - метод получение одного товара по его id
-console.log('\n 2 Найден товар с ID из главной страницы: ', list.findGoodByID("c101ab44-ed99-4a54-990d-47aa2bb4e7d9"));
+// // findGoodByID - метод получение одного товара по его id
+// console.log('\n 2 Найден товар с ID из главной страницы: ', list.findGoodByID("c101ab44-ed99-4a54-990d-47aa2bb4e7d9"));
 
-// setToShow - метод для сохранения товара для подробного отображения
-list.setToShow(list.findGoodByID("c101ab44-ed99-4a54-990d-47aa2bb4e7d9"));
-console.log('\n 3 Товар сохранен для подробного отображения: ', list.findGoodByID("c101ab44-ed99-4a54-990d-47aa2bb4e7d9"));
+// // setToShow - метод для сохранения товара для подробного отображения
+// list.setToShow(list.findGoodByID("c101ab44-ed99-4a54-990d-47aa2bb4e7d9"));
+// console.log('\n 3 Товар сохранен для подробного отображения: ', list.findGoodByID("c101ab44-ed99-4a54-990d-47aa2bb4e7d9"));
 
-// getToShow - метод для получение товара для подробного отображения
-console.log('\n 4 Вывод товара для подробного отображения: ', list.getToShow());
+// // getToShow - метод для получение товара для подробного отображения
+// console.log('\n 4 Вывод товара для подробного отображения: ', list.getToShow());
 
 
 
-console.log('\n\n\nТЕСТ КЛАССА Basket');
-const basket = new Basket();
-// setItemToBascet -  метод для добавления товара, который был получен в параметре в массив корзины
-basket.setItem(list.findGoodByID("c101ab44-ed99-4a54-990d-47aa2bb4e7d9"));
-console.log('1 Товар добавлен в корзину:', list.findGoodByID("c101ab44-ed99-4a54-990d-47aa2bb4e7d9"));
-basket.setItem(list.findGoodByID("854cef69-976d-4c2a-a18c-2aa45046c390"));
-console.log('1 Товар добавлен в корзину:', list.findGoodByID("854cef69-976d-4c2a-a18c-2aa45046c390"));
+// console.log('\n\n\nТЕСТ КЛАССА Basket');
+// const basket = new Basket();
+// // setItemToBascet -  метод для добавления товара, который был получен в параметре в массив корзины
+// basket.setItem(list.findGoodByID("c101ab44-ed99-4a54-990d-47aa2bb4e7d9"));
+// console.log('1 Товар добавлен в корзину:', list.findGoodByID("c101ab44-ed99-4a54-990d-47aa2bb4e7d9"));
+// basket.setItem(list.findGoodByID("854cef69-976d-4c2a-a18c-2aa45046c390"));
+// console.log('1 Товар добавлен в корзину:', list.findGoodByID("854cef69-976d-4c2a-a18c-2aa45046c390"));
 
-// getItemsFromBascet - метод для получения товаров, которые находятся в корзине
-console.log("\n2 Содержимое корзины", basket.getItems());
+// // getItemsFromBascet - метод для получения товаров, которые находятся в корзине
+// console.log("\n2 Содержимое корзины", basket.getItems());
 
-// removeItemFromBascet - метод для удаления товара, полученного в параметре из массива корзины
-basket.removeItem(list.findGoodByID("854cef69-976d-4c2a-a18c-2aa45046c390"));
-console.log('\n3 Товар удален из корзины:', list.findGoodByID("854cef69-976d-4c2a-a18c-2aa45046c390"));
-console.log("3 Содержимое корзины", basket.getItems());
+// // removeItemFromBascet - метод для удаления товара, полученного в параметре из массива корзины
+// basket.removeItem(list.findGoodByID("854cef69-976d-4c2a-a18c-2aa45046c390"));
+// console.log('\n3 Товар удален из корзины:', list.findGoodByID("854cef69-976d-4c2a-a18c-2aa45046c390"));
+// console.log("3 Содержимое корзины", basket.getItems());
 
-// calculateSumm - метод для подсчета суммы 
-console.log("4 Сумма заказа: ", basket.calculateSumm());
+// // calculateSumm - метод для подсчета суммы 
+// console.log("4 Сумма заказа: ", basket.calculateSumm());
 
-// clearBascet - метод для очистки корзины
-basket.clear();
-console.log("\n5 Корзина очищена");
-console.log("5 Содержимое корзины", basket.getItems());
+// // clearBascet - метод для очистки корзины
+// basket.clear();
+// console.log("\n5 Корзина очищена");
+// console.log("5 Содержимое корзины", basket.getItems());
 
-// countItems - метод для получения количества товаров в корзине
-console.log("\n6 Количество товаров в корзине: ", basket.countItems());
+// // countItems - метод для получения количества товаров в корзине
+// console.log("\n6 Количество товаров в корзине: ", basket.countItems());
 
-// isInBascet - метод для проверки наличия товара в корзине по его id, полученному в параметр метода 
-console.log("\n7 Товар в корзине: ", basket.isInBascet("854cef69-976d-4c2a-a18c-2aa45046c390"));
+// // isInBascet - метод для проверки наличия товара в корзине по его id, полученному в параметр метода 
+// console.log("\n7 Товар в корзине: ", basket.isInBascet("854cef69-976d-4c2a-a18c-2aa45046c390"));
 
 
 
 console.log('\n\n\nТЕСТ КЛАССА Buyer');
 const buyer = new Buyer();
-// set(Payment/Address/Email/Phone) - методы для сохранения данных в соответствующие поля
-buyer.setPayment("cash");
-console.log("1 Проверка валидации: " , buyer.validation());
-buyer.setAddress("Night");
-buyer.setEmail("@");
-buyer.setPhone("89");
+// // set(Payment/Address/Email/Phone) - методы для сохранения данных в соответствующие поля
+// buyer.setPayment("cash");
+// console.log("1 Проверка валидации: " , buyer.validation());
+// buyer.setAddress("Night");
+// buyer.setEmail("@");
+// buyer.setPhone("89");
 
-// get(Payment/Address/Email/Phone) - методы для получения данных из соответствующих полей
-console.log('\n2 Вывод данных по отдельности');
-console.log("Адрес: " , buyer.getAddress());
-console.log("Почта: ", buyer.getEmail());
-console.log("Способ оплаты: ", buyer.getPayment());
-console.log("Телефон: ", buyer.getPhone());
+// // get(Payment/Address/Email/Phone) - методы для получения данных из соответствующих полей
+// console.log('\n2 Вывод данных по отдельности');
+// console.log("Адрес: " , buyer.getAddress());
+// console.log("Почта: ", buyer.getEmail());
+// console.log("Способ оплаты: ", buyer.getPayment());
+// console.log("Телефон: ", buyer.getPhone());
 
-// getOrderData - метод для получения значений поле класса Buyer
-console.log("\n3 Вывод всех данных: ", buyer.getOrderData());
+// // getOrderData - метод для получения значений поле класса Buyer
+// console.log("\n3 Вывод всех данных: ", buyer.getOrderData());
 
-// clear - метод для очистки поле класса Buyer
-buyer.clear();
-console.log("\n4 Данные очищены: ", buyer.getOrderData());
+// // clear - метод для очистки поле класса Buyer
+// buyer.clear();
+// console.log("\n4 Данные очищены: ", buyer.getOrderData());
 
 
 console.log('\n\n\nТЕСТ КЛАССА Requests');
@@ -91,26 +93,61 @@ const api = new Api(API_URL);
 const testApi = new Requests(api);
 const products = testApi.getGoods();
 
-products.then(res => {
+await products.then(res => {
   productsFromServer.setItems(res);
 })
 .catch(err => console.log("Ошибка", err));
 
 console.log("Товары с сервера: ", productsFromServer.getItems());
 
-buyer.setPayment("cash");
-buyer.setAddress("Night");
-buyer.setEmail("@");
-buyer.setPhone("89");
+const cards = productsFromServer.getItems();
+const cardTemplate = ensureElement<HTMLTemplateElement>('#card-catalog');
 
-newBascet.setItem(list.findGoodByID("c101ab44-ed99-4a54-990d-47aa2bb4e7d9"));
-console.log("Товары в корзине: ", newBascet.getItems());
-console.log("Сумма заказа: ", newBascet.calculateSumm());
 
-const resultFromServer = testApi.postOrder(buyer.getOrderData(), newBascet.getItems(), newBascet.calculateSumm());
-resultFromServer.then(res => {
-  console.log("\n Сервер вернул сообщение:");
+// const page = new Page(document.body);
+import {CDN_URL} from './utils/constants.ts'
 
-  console.log("ID заказа: ", res.id);
-  console.log("Сумма заказа: ", res.total);
-}).catch(err => console.log("Ошибка", err));
+const items = [];
+
+cards.forEach(item => {
+  const card = new Card(cloneTemplate(cardTemplate));
+  items.push(card.render(
+    {
+      title: item.title,
+			image: `${CDN_URL}${item.image}`,
+			price: item.price,
+			category: item.category,
+    }
+  ))
+})
+
+
+const start = ensureElement<HTMLElement>('.gallery');
+start.replaceChildren(...items)
+// class Page {
+
+//   catalog: HTMLElement;
+
+// 	constructor(container: HTMLElement) {
+// 		// super(container);
+//     this.catalog = ensureElement<HTMLElement>('.gallery');
+//   }
+// }
+
+
+
+// buyer.setPayment("cash");
+// buyer.setAddress("Night");
+// buyer.setEmail("@");
+// buyer.setPhone("89");
+
+// newBascet.setItem(list.findGoodByID("c101ab44-ed99-4a54-990d-47aa2bb4e7d9"));
+// console.log("Товары в корзине: ", newBascet.getItems());
+// console.log("Сумма заказа: ", newBascet.calculateSumm());
+
+// const resultFromServer = testApi.postOrder(buyer.getOrderData(), newBascet.getItems(), newBascet.calculateSumm());
+// resultFromServer.then(res => {
+//   console.log("\nСервер вернул сообщение:");
+//   console.log("ID заказа: ", res.id);
+//   console.log("Сумма заказа: ", res.total);
+// }).catch(err => console.log("Ошибка", err));
